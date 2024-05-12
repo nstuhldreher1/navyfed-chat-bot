2b8df270ffa345e6d468198e59e4b3a953278b72
nstuhldreher1 <92557643+nstuhldreher1@users.noreply.github.com>
2024-05-11 22:50:59 -0400

    feat: navy fed header

---
+++ chat-bot/src/components/NavyFedHeader.jsx
@@ -0,0 +1,12 @@
+import './NavyFedHeader.css'
+
+function NavyFedHeader(){
+    return(
+        <div className = "NavyFedHeader">
+            <p id = "NavyFed">NAVY FEDERAL</p>
+            <p id= "FAQ">FAQ</p>
+        </div>
+    );
+}
+
+export default NavyFedHeader;
